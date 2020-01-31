6c7b5ccc13f206ea9696db058ae12e51f67f2b26
Ellis Hughes <ellishughes@live.com>
2020-01-30 21:44:06 -0800

    correct reference number
    lubridate::wday starts with monday = 1, not sunday.

--- R/next_tuesday.R
+++ R/next_tuesday.R
@@ -6,7 +6,7 @@
 next_tuesday <- function() {
     todays_date <- lubridate::today(tz = "America/New_York")
 
-    diff_tuesday <- 3 - lubridate::wday(todays_date)
+    diff_tuesday <- 2 - lubridate::wday(todays_date)
 
     if (diff_tuesday < 0) {
         diff_tuesday <- diff_tuesday + 7
