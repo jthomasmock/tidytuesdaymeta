120cb2929e9c4abf4bc07086ed1aba4a9c96916d
Thomas Mock <j.thomasmock@gmail.com>
2021-03-25 20:16:00 -0500

    add alt text to all images for announcement

--- R/post_tidytuesday.R
+++ R/post_tidytuesday.R
@@ -9,7 +9,7 @@
 #'
 
 
-post_tidytuesday <- function(exploring, short_link) {
+post_tidytuesday <- function(exploring, short_link, alt_add) {
 
   week_num <- tidytuesdaymeta::next_week_num()
   week_date <- tidytuesdaymeta::next_tuesday()
@@ -17,6 +17,7 @@ post_tidytuesday <- function(exploring, short_link) {
 
   # post the tweet with fill
   rtweet::post_tweet(
+    token = readRDS("~/Library/Caches/rtweet/auth.rds"),
     status = glue::glue(
       "The @R4DScommunity welcomes you to week {week_num} of #TidyTuesday!  We're exploring {exploring}!
 
@@ -37,6 +38,18 @@ post_tidytuesday <- function(exploring, short_link) {
       here::here("static_img", "tt_rules.png"),
       here::here(tues_year, week_date, "pic1.png"),
       here::here(tues_year, week_date, "pic2.png")
-    )
+    ),
+media_alt_text = c(
+  "Logo for the #TidyTuesday Project, it's the words TidyTuesday overlaying a black paint splash",
+  "The data set comes from the source article or the source that the article credits. Be mindful that the data is what it is and Tidy Tuesday is designed to help you practice data visualization and basic data wrangling in R.
+This is NOT about criticizing the original article or graph. Real people made the graphs, collected or acquired the data! Focus on the provided dataset, learning, and improving your techniques in R.
+This is NOT about criticizing or tearing down your fellow #RStats practitioners or their code! Be supportive and kind to each other! Like other's posts and help promote the #RStats community!
+Use the hashtag #TidyTuesday on Twitter if you create your own version and would like to share it.
+Include a picture of the visualisation.
+Include a copy of the code used to create your visualization.
+Focus on improving your craft, even if you end up with something simple!
+Give credit to the original data source whenever possible.",
+alt_add
+)
   )
 }
